3a9511b8c3b25b79ffa52244dbe078e3117b98a8
Ty Everett <ty@tyweb.us>
2022-07-17 22:21:29 -0700

    Dockerize

---
+++ reader/build-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t us.gcr.io/computing-with-integrity/convo-cump-reader .
+docker push us.gcr.io/computing-with-integrity/convo-cump-reader
---
+++ transformer/build-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t us.gcr.io/computing-with-integrity/convo-cump-transformer .
+docker push us.gcr.io/computing-with-integrity/convo-cump-transformer
